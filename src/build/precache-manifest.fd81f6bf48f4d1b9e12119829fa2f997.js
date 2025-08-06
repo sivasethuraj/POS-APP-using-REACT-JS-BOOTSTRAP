da6ec9842c88366a49eb9e9ae243fb54b7c5490a
SIVA <93249272+sivasethuraj@users.noreply.github.com>
2025-08-06 12:17:00 +0530

    added build files (#5)
    whoever want to see this project you can directly run from this build directory
    for that you need to follow below step:
    1. install serve package from npm
    ``` npm install -g serve ```
    2. then serve static files 
    ``` serve -s build ```

---
+++ src/build/precache-manifest.fd81f6bf48f4d1b9e12119829fa2f997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c256125636e55b2ed797",
+    "url": "/static/css/main.b8da8de8.chunk.css"
+  },
+  {
+    "revision": "c256125636e55b2ed797",
+    "url": "/static/js/main.72cf6edc.chunk.js"
+  },
+  {
+    "revision": "c81066000cd0dffd9c96",
+    "url": "/static/js/runtime~main.447739d8.js"
+  },
+  {
+    "revision": "673b3475f6ca2f1b5275",
+    "url": "/static/css/2.f4290bcf.chunk.css"
+  },
+  {
+    "revision": "673b3475f6ca2f1b5275",
+    "url": "/static/js/2.f0b0df51.chunk.js"
+  },
+  {
+    "revision": "03401dfc1b6d28949ba8",
+    "url": "/static/js/3.779d2b58.chunk.js"
+  },
+  {
+    "revision": "190a958c525b4ca244091e0b6bcf6b41",
+    "url": "/index.html"
+  }
+];
